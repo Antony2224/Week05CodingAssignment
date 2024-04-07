50957768c6af6b94c986d0a89b72a6c355989ac8
Antony2224 <165442654+Antony2224@users.noreply.github.com>
2024-04-06 21:20:59 -0600

    AsteriskLogger.java

--- src/AsteriskLogger.java
+++ src/AsteriskLogger.java
@@ -1,12 +1,12 @@
 //Class that calls in the Logger methods and creates new methods that are class specific
 public class AsteriskLogger implements Logger{
-
+	//Overwritten method from Logger interface
 	@Override
 	public void log(String log) {
 		// TODO Auto-generated method stub
 		System.out.println("***" + log + "***");
 	}
-
+	//Overwritten method from Logger interface
 	@Override
 	public void error(String error) {
 		// TODO Auto-generated method stub
